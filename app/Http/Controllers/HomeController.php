0ba74218d9bb5cc26ec91b0f72e611160e184de1
tiansetiaone <107497446+tiansetiaone@users.noreply.github.com>
2023-08-05 22:10:21 +0700

    HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,21 +8,6 @@ use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index(){
         $client = new Client();
         $response = $client->request('GET','https://newsapi.org/v2/everything?q=tesla&from=2023-07-05&sortBy=publishedAt&apiKey=95342992d8414c5abb5e000e6ed038e2');
