d53846d41891cffa4f3ed458b4b02feaec9a37a6
tiansetiaone <107497446+tiansetiaone@users.noreply.github.com>
2023-08-08 08:49:21 +0700

    HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index(){
         $client = new Client();
-        $response = $client->request('GET','https://newsapi.org/v2/everything?q=tesla&from=2023-07-05&sortBy=publishedAt&apiKey=95342992d8414c5abb5e000e6ed038e2');
+        $response = $client->request('GET','https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=95342992d8414c5abb5e000e6ed038e2');
         $body = $response->getBody()->getContents();
         $data = json_decode($body, true);
         // return ($data['articles']);
